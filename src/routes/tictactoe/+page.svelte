3f9f51e87c11e6cfff7e7c33c5775e26e9b94bc5
Florian Schmid <melonencool@gmail.com>
2024-02-28 08:58:16 +0100

    add button for creating a game

--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+    import { getSession } from "$lib/sessionManager";
+    import { goto } from '$app/navigation';
 
+    function createGame(){
+        fetch("tictactoe/api/?session=" + getSession())
+        .then(response => {
+            response.text().then((link: string) => {
+                goto(link.substring(1, link.length - 1));
+            })
+        });
+    }
 </script>
 
-<button>
+<button on:click={() => createGame()} class="px-2 h-10 w-fit rounded-full bg-slate-300 hover:bg-slate-400">
     Create Lobby
 </button>
